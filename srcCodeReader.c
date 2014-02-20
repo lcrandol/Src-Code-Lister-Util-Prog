a0bf9477f477f003f8c0ca34d50aa2adae7e4ebf
lcrandol <levi_roble@yahoo.com>
2014-02-20 11:49:22 -0700

    Just trying to start writing the main
    I figured a character pointer type will work just fine for the name of
    the file. We still have a good while to go, but here is a start.

---
+++ srcCodeReader.c
@@ -0,0 +1,9 @@
+/* Source Code Reader Utility Program */
+
+#include<stdio.h>
+
+/* This main function will accept a char* arg for a name of the file to be read in. (Levi) */
+int main(char* name_of_file)
+{
+    printf(*name_of_file);
+}
